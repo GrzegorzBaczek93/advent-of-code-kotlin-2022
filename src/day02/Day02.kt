35f0cffde52c82c27f638ea992573f44c8f24e46
Grzegorz Baczek <grzegorz.baczek.93@gmail.com>
2022-12-02 11:59:02 +0100

    day 2 refactored

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -13,32 +13,62 @@ fun main() {
     withStopwatch { println(part2(input)) }
 }
 
-private fun part1(input: List<String>) = input.sumOf { it.resolvePointsPart1() }
-
-private fun part2(input: List<String>) = input.sumOf { it.resolvePointsPart2() }
-
-private fun String.resolvePointsPart1() = when (this) {
-    "A X" -> 4 // Rock - Rock = draw
-    "A Y" -> 8 // Rock - Paper = win
-    "A Z" -> 3 // Rock - Scissors = lose
-    "B X" -> 1 // Paper - Rock = lose
-    "B Y" -> 5 // Paper - Paper = draw
-    "B Z" -> 9 // Paper - Scissors = win
-    "C X" -> 7 // Scissors - Rock = win
-    "C Y" -> 2 // Scissors - Paper = lose
-    "C Z" -> 6 // Scissors - Scissors = draw
-    else -> throw UnsupportedOperationException()
+private fun part1(input: List<String>) = input.sumOf { game ->
+    calculateResult(game.first().toShape(), game.last().toShape())
 }
 
-private fun String.resolvePointsPart2() = when (this) {
-    "A X" -> 3 // Rock - Scissors = lose
-    "A Y" -> 4 // Rock - Rock = draw
-    "A Z" -> 8 // Rock - Paper = win
-    "B X" -> 1 // Paper - Rock = lose
-    "B Y" -> 5 // Paper - Paper = draw
-    "B Z" -> 9 // Paper - Scissors = win
-    "C X" -> 2 // Scissors - Paper = lose
-    "C Y" -> 6 // Scissors - Scissors = draw
-    "C Z" -> 7 // Scissors - Rock = win
-    else -> throw UnsupportedOperationException()
+private fun part2(input: List<String>) = input.sumOf { game ->
+    val shapeA = game.first().toShape()
+    calculateResult(shapeA, game.last().toShape(shapeA.winningShape, shapeA.drawingShape, shapeA.losingShape))
 }
+
+private fun Char.toShape(x: Shape = Shape.Rock, y: Shape = Shape.Paper, z: Shape = Shape.Scissors) = when (this) {
+    'A' -> Shape.Rock
+    'B' -> Shape.Paper
+    'C' -> Shape.Scissors
+    'X' -> x
+    'Y' -> y
+    'Z' -> z
+    else -> throw IllegalArgumentException()
+}
+
+private enum class Shape(val points: Int) {
+    Rock(1) {
+        override val winningShape: Shape
+            get() = Scissors
+
+        override val losingShape: Shape
+            get() = Paper
+    },
+    Paper(2) {
+        override val winningShape: Shape
+            get() = Rock
+
+        override val losingShape: Shape
+            get() = Scissors
+    },
+    Scissors(3) {
+        override val winningShape: Shape
+            get() = Paper
+
+        override val losingShape: Shape
+            get() = Rock
+    };
+
+    abstract val winningShape: Shape
+    abstract val losingShape: Shape
+    val drawingShape: Shape
+        get() = this
+}
+
+private enum class MatchResult(val points: Int) {
+    Win(6), Draw(3), Lose(0);
+}
+
+private fun calculateResult(shapeA: Shape, shapeB: Shape): Int =
+    when (shapeB) {
+        shapeA.winningShape -> MatchResult.Lose.points + shapeB.points
+        shapeA.drawingShape -> MatchResult.Draw.points + shapeB.points
+        shapeA.losingShape -> MatchResult.Win.points + shapeB.points
+        else -> throw UnsupportedOperationException()
+    }
