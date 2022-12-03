8c777e06199bb16c07e36c8b569e0c8c483093a9
Grzegorz Baczek <grzegorz.baczek.93@gmail.com>
2022-12-03 09:09:26 +0100

    day 3

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -13,6 +13,42 @@ fun main() {
     withStopwatch { println(part2(input)) }
 }
 
-private fun part1(input: List<String>) = input
+private fun part1(input: List<String>) = input.sumOf { it.findCommon().asNumber() }
 
-private fun part2(input: List<String>) = input
+private fun part2(input: List<String>) = input.chunked(3).sumOf { it.findCommon().asNumber() }
+
+private fun String.findCommon(): Char {
+    val firstSlice = take(length / 2).toSet()
+
+    takeLast(length / 2).forEach {
+        if (firstSlice.contains(it)) {
+            return it
+        }
+    }
+    error("No common element for input")
+}
+
+private fun List<String>.findCommon(): Char {
+    val firstSlice = get(0).toSet()
+    val commonElements = mutableSetOf<Char>()
+
+    get(1).forEach {
+        if (firstSlice.contains(it)) {
+            commonElements.add(it)
+        }
+    }
+    get(2).forEach {
+        if (commonElements.contains(it)) {
+            return it
+        }
+    }
+    error("No common element for input")
+}
+
+private fun Char.asNumber(): Int {
+    return if (this.isLowerCase()) {
+        this.code - 96
+    } else {
+        this.code - 38
+    }
+}
