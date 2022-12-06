ebfc24b3eda5394d9861ed5cbbb076777560ade1
Grzegorz Baczek <grzegorz.baczek.93@gmail.com>
2022-12-06 09:27:57 +0100

    day 6

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -2,17 +2,31 @@ package day06
 
 import readInput
 import utils.withStopwatch
+import utils.containsDuplicates
 
 fun main() {
-    val testInput = readInput("input05_test")
+    val testInput = readInput("input06_test")
     withStopwatch { println(part1(testInput)) }
     withStopwatch { println(part2(testInput)) }
 
-    val input = readInput("input05")
+    val input = readInput("input06")
     withStopwatch { println(part1(input)) }
     withStopwatch { println(part2(input)) }
 }
 
-private fun part1(input: List<String>) = input
+private fun part1(input: List<String>) = input.first().solve(4)
 
-private fun part2(input: List<String>) = input
+private fun part2(input: List<String>) = input.first().solve(14)
+
+private fun String.solve(packageSize: Int): Int {
+    var startIndex = 0
+    while (startIndex + packageSize <= length) {
+        val chunk = substring(startIndex, startIndex + packageSize)
+        if (chunk.containsDuplicates().not()) {
+            return startIndex + packageSize
+        }
+        startIndex++
+    }
+
+    return -1
+}
--- src/utils/StringUtils.kt
+++ src/utils/StringUtils.kt
@@ -2,3 +2,5 @@ package utils
 
 fun String.toRange(): IntRange =
     split("-").let { (first, second) -> Integer.parseInt(first)..Integer.parseInt(second) }
+
+inline fun String.containsDuplicates(): Boolean = toSet().size != this.length
